7ea17cae42dbadcefa8a4582402fd9ccd69b5e15
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2025-11-11 18:46:28 -0300

    Ajuste no arquivo de inicialização e nos icones

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -6,20 +7,24 @@ db = SQLAlchemy()
 csrf = CSRFProtect()
 
 def create_app():
-    app = Flask(__name__)
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+
+    app = Flask(
+        __name__,
+        static_folder=os.path.join(base_dir, "static"),
+        template_folder=os.path.join(base_dir, "templates")
+    )
     app.config.from_object('app.config.Config')
     
     db.init_app(app)
     csrf.init_app(app)
     
-    # Registrar Blueprints
     from .views import main
     from .admin import admin
-    
+
     app.register_blueprint(main)
     app.register_blueprint(admin, url_prefix='/admin')
     
-    # Template filter
     from .utils import strip_leading_whitespace
     app.template_filter('strip_leading_whitespace')(strip_leading_whitespace)
     
